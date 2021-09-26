f62475a55695d0fad3e7c172ce26a01c22d9e6f0
vmarc <marc@vmarc.be>
2021-09-26 20:01:58 +0200

    audio spike - bump angular version

---
+++ vaadin/src/main/java/vaadin/views/MainLayout.java
@@ -0,0 +1,14 @@
+package vaadin.views;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.contextmenu.MenuItem;
+import com.vaadin.flow.component.contextmenu.SubMenu;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.menubar.MenuBar;
+
+public class MainLayout {
+
+}
