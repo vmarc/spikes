90dade13cc5d348e02d53aa74d7cedb0daa29c09
vmarc <marc@vmarc.be>
2021-11-04 23:01:45 +0200

    translations spike - Angular 13

--- translations/src/app/app.component.ts
+++ translations/src/app/app.component.ts
@@ -3,9 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <p i18n="@@translations.one">one {{name}}</p>
-    <p i18n="@@translations.two">two {{name}}</p>
-    <p i18n="@@translations.three">three {{name}}:name:</p>
+    <p i18n="@@translations.one">one {{firstField}} {{secondField}}</p>
+    <p i18n="@@translations.two">two {{secondField}} {{firstField}}</p>
+    <p i18n="@@translations.three">three</p>
     <p>{{four()}}</p>
     <p>{{five()}}</p>
     <p i18n="@@translations.six">
@@ -17,13 +17,14 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  name = "marc";
+  firstField = "first";
+  secondField = "second";
 
   four(): string {
     return $localize`:@@translations.four:four`;
   }
 
   five(): string {
-    return $localize`:@@translations.five:five ${this.name}:name:`;
+    return $localize`:@@translations.five:five ${this.firstField}:first: ${this.secondField}:second:`;
   }
 }
--- translations/src/app/app.module.ts
+++ translations/src/app/app.module.ts
@@ -1,17 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
