0b0eacc6d787862253f92f1f2ae7a9cf8cbd4d02
vmarc <marc@vmarc.be>
2021-11-12 20:43:09 +0100

    translations spike - date localization

--- translations/src/app/app.component.ts
+++ translations/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Inject, LOCALE_ID} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +13,53 @@ import {Component} from '@angular/core';
       six
 
     </p>
+    <p>
+      now: {{now | date}}
+    </p>
+    <p>
+      now(en): {{now | date:'short':undefined:'en'}}
+    </p>
+    <p>
+      now(fr): {{now | date:'short':undefined:'fr'}}
+    </p>
+    <p>
+      now(nl): {{now | date:'short':undefined:'nl'}}
+    </p>
+    <p>
+      now(de): {{now | date:'short':undefined:'de'}}
+    </p>
+    <p>
+      value: {{value | number}}
+    </p>
+    <p>
+      value (fr): {{value | number:'0.0':'fr'}}
+    </p>
+    <p>
+      value (nl): {{value | number:'0.0':'nl'}}
+    </p>
+    <p>
+      value (de): {{value | number:'0.0':'de'}}
+    </p>
+    <p>
+      locale: {{locale}}
+    </p>
+
+    <p>
+      languages: <span *ngFor="let language of languages()">{{language}} </span>
+    </p>
   `,
 })
 export class AppComponent {
 
   firstField = "first";
   secondField = "second";
+  now = new Date();
+  value = 1234567;
+
+  constructor(
+    @Inject(LOCALE_ID) public locale: string
+  ) {
+  }
 
   four(): string {
     return $localize`:@@translations.four:four`;
@@ -27,4 +68,9 @@ export class AppComponent {
   five(): string {
     return $localize`:@@translations.five:five ${this.firstField}:first: ${this.secondField}:second:`;
   }
+
+  languages(): ReadonlyArray<string> {
+    return navigator.languages;
+  }
+
 }
--- translations/src/app/app.module.ts
+++ translations/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import '@angular/common/locales/global/fr';
+import '@angular/common/locales/global/nl';
+import '@angular/common/locales/global/de';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
