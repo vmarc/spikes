d51f03b7d5ddb452731087bcd23881c91f69cdaa
vmarc <marc@vmarc.be>
2021-12-22 21:29:33 +0100

    lazy spike - Angular 13

--- lazy/src/app/main-feature/main-feature.component.ts
+++ lazy/src/app/main-feature/main-feature.component.ts
@@ -8,10 +8,10 @@ import {Component} from '@angular/core';
         Main Feature (router lazy loaded)
       </div>
       <p>
-        <a [routerLink]="" (click)="selection = 'feature1'">Feature 1</a> |
-        <a [routerLink]="" (click)="selection = 'feature2'">Feature 2</a> |
-        <a [routerLink]="" (click)="selection = 'feature3'">Feature 3</a> |
-        <a [routerLink]="" (click)="selection = 'feature4'">Feature 4</a>
+        <a [routerLink]="[]" (click)="selection = 'feature1'">Feature 1</a> |
+        <a [routerLink]="[]" (click)="selection = 'feature2'">Feature 2</a> |
+        <a [routerLink]="[]" (click)="selection = 'feature3'">Feature 3</a> |
+        <a [routerLink]="[]" (click)="selection = 'feature4'">Feature 4</a>
       </p>
       <p *ngIf="selection.length === 0">Click feature link</p>
       <app-feature-1-lazy-load *ngIf="selection === 'feature1'"></app-feature-1-lazy-load>
