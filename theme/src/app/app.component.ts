4a4a5d451563b8510e2757ef5a41a4ac4c2fc576
vmarc <marc@vmarc.be>
2024-04-01 09:13:54 +0100

    theme spike install angular material

--- theme/src/app/app.component.ts
+++ theme/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: true,
-    imports: [NgSwitch, NgSwitchDefault, NgSwitchCase]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
 })
-export class AppComponent {
-  title = 'theme';
-}
+export class AppComponent {}
--- theme/src/main.ts
+++ theme/src/main.ts
@@ -1,12 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import {importProvidersFrom} from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
+    providers: [importProvidersFrom(BrowserModule, BrowserAnimationsModule)]
 })
   .catch(err => console.error(err));
