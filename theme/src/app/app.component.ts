615fae6afe4fcd9974969fb2897ddcc65414f85f
vmarc <marc@vmarc.be>
2024-04-02 10:04:49 +0200

    theme spike add components

--- theme/src/app/app.component.ts
+++ theme/src/app/app.component.ts
@@ -2,12 +2,24 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+  ],
 })
-export class AppComponent {}
+export class AppComponent {
+  setTheme($event: MatSelectChange) {}
+}
